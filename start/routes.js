348770f988119db7e3439bc6d13eff1e9d722525
Claudianus Mamertus <claudianus@engineer.com>
2019-01-03 02:13:08 +0900

    @

--- start/routes.js
+++ start/routes.js
@@ -19,3 +19,5 @@ const Route = use('Route')
 Route.get('/', 'PageController.index')
 Route.get('/json', 'ApiController.json')
 Route.get('/json/:ip', 'ApiController.jsonParam')
+
+Route.get('dev', ({request}) => request.ips())
